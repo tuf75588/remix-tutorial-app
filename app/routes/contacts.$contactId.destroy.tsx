338bb0770515af7925b6d50e57266b073641e37c
Andrew Davis <develop.it4@gmail.com>
2024-04-18 12:58:32 -0400

    Fix formatting in contacts.$contactId.destroy.tsx

--- app/routes/contacts.$contactId.destroy.tsx
+++ app/routes/contacts.$contactId.destroy.tsx
@@ -1,6 +1,9 @@
 import { ActionFunctionArgs, redirect } from '@remix-run/node';
 import invariant from 'tiny-invariant';
 import { deleteContact } from '~/data';
+
+
+// this runs after the "destroy" action has been run on the form relative to this file.
 export const action = async ({ params }: ActionFunctionArgs) => {
   invariant(params.contactId, 'Missing contact Id');
   await deleteContact(params.contactId);
